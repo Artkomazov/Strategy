89afcb4c531acc92fdf35062be6b6355aa2f4105
Artkomazov <artkomazov@gmail.com>
2023-06-02 19:25:51 +0200

    added unit selection using frame, added states (selection, frame, other) using enum

--- Assets/Scripts/Managment.cs
+++ Assets/Scripts/Managment.cs
@@ -1,12 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum SelectionState
+{
+    UnitsSelected,
+    Frame,
+    Other
+}
 
 public class Managment : MonoBehaviour
 {
+    [SerializeField] private SelectionState _currentSelectionState;
+
     [SerializeField] private Camera _camera;
     [SerializeField] private SelectableOdject _hovered;
     [SerializeField] private List<SelectableOdject> _listOfSelected;
+    [SerializeField] private Image _frameImage;
+    private Vector2 _frameStart;
+    private Vector2 _frameEnd;
 
     private void Update()
     {
@@ -52,31 +64,83 @@ public class Managment : MonoBehaviour
                 {
                     UnSelectAll();
                 }
+                _currentSelectionState = SelectionState.UnitsSelected;
                 Select(_hovered);
-            }
-            //else
-            //{
-            //    UnSelectAll();
-            //}
-
-            if (hit.collider.tag == "Ground")
-            {
-                for (int i = 0; i < _listOfSelected.Count; i++)
-                {
-                    _listOfSelected[i].WhenClickOnGround(hit.point);
-                }
             }
         }
 
+        if (_currentSelectionState == SelectionState.UnitsSelected)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    for (int i = 0; i < _listOfSelected.Count; i++)
+                    {
+                        _listOfSelected[i].WhenClickOnGround(hit.point);
+                    }
+                }
+            }
+        }
+        
+           
         if (Input.GetMouseButtonUp(1)) {
             UnSelectAll();
+        }
+
+
+        // Frame Selection
+        if (Input.GetMouseButtonDown(0))
+        {
+            _frameStart = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            _frameEnd = Input.mousePosition;
+            Vector2 min = Vector2.Min(_frameStart, _frameEnd);
+            Vector2 max = Vector2.Max(_frameStart, _frameEnd);
+            Vector2 sizeFrame = max - min;
+
+            if (sizeFrame.magnitude > 10)
+            {
+                _frameImage.enabled = true;
+                _frameImage.rectTransform.anchoredPosition = min;
+                _frameImage.rectTransform.sizeDelta = sizeFrame;
+                Rect rect = new Rect(min, sizeFrame);
+
+                UnSelectAll();
+                Unit[] allUnits = FindObjectsOfType<Unit>();
+                for (int i = 0; i < allUnits.Length; i++)
+                {
+                    Vector2 screenPosition = _camera.WorldToScreenPoint(allUnits[i].transform.position);
+                    if (rect.Contains(screenPosition))
+                    {
+                        Select(allUnits[i]);
+                    }
+                }
+                _currentSelectionState = SelectionState.Frame;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _frameImage.enabled = false;
+            if (_listOfSelected.Count > 0)
+            {
+                _currentSelectionState = SelectionState.UnitsSelected;
+            }
+            else 
+            {
+                _currentSelectionState = SelectionState.Other;
+            }
         }
     }
 
     private void Select(SelectableOdject selectableOdject) {
         if (_listOfSelected.Contains(selectableOdject) == false) {
             _listOfSelected.Add(selectableOdject);
-            _hovered.Select();
+            selectableOdject.Select();
         }
     }
     private void UnSelectAll()
@@ -86,6 +150,7 @@ public class Managment : MonoBehaviour
             _listOfSelected[i].UnSelect();
         }
         _listOfSelected.Clear();
+        _currentSelectionState = SelectionState.Other;
     }
     private void UnhoverCurrent()
     {
