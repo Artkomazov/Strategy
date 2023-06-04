8b4183c656cb54f2c9e1e56158fa505d5887a0ab
Artkomazov <artkomazov@gmail.com>
2023-06-04 13:14:38 +0200

    added placement of buildings on the grid

--- Assets/Scripts/Building/BuildingPlacer.cs
+++ Assets/Scripts/Building/BuildingPlacer.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class BuildingPlacer : MonoBehaviour
 {
     public float CellSize = 1f;
+
+    [SerializeField] private Camera _ñamera;
+    [SerializeField] private Building _currentBuilding;
+    private Plane _plane;
+
+
+    void Start()
+    {
+        _plane = new Plane(Vector3.up, Vector3.zero);
+    }
+    void Update()
+    {
+        if (_currentBuilding == null)
+        {
+            return;
+        }
+
+        Ray ray = _ñamera.ScreenPointToRay(Input.mousePosition);
+        float distanse;
+        _plane.Raycast(ray, out distanse);
+        Vector3 point = ray.GetPoint(distanse) / CellSize;
+
+        int x = Mathf.RoundToInt(point.x);
+        int z = Mathf.RoundToInt(point.z);
+
+        _currentBuilding.transform.position = new Vector3(x,0,z) * CellSize;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentBuilding = null;
+        }
+    }
 }
